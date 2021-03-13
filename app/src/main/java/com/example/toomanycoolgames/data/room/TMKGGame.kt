ddf219dd55113bb4f2ee505e2bb773b1d3bf5e49
Jordan Wilson <wilsonfraserjordan@gmail.com>
2021-03-13 09:05:54 -0800

    storing data for release dates (still need to test on emulator)

--- app/src/main/java/com/example/toomanycoolgames/data/room/TMKGGame.kt
+++ app/src/main/java/com/example/toomanycoolgames/data/room/TMKGGame.kt
@@ -1,8 +1,6 @@
 package com.example.toomanycoolgames.data.room
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.room.*
 
 @Entity
 data class TMKGGame(
@@ -11,5 +9,24 @@ data class TMKGGame(
     @ColumnInfo(name = "igdb_id") val igdbId: Long,
     @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "cover_id") val coverId: String,
-    @ColumnInfo(name = "summary") val summary: String
+    @ColumnInfo(name = "summary") val summary: String,
+
+    )
+
+data class TMKGGameWithReleaseDates(
+    @Embedded val game: TMKGGame,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "game_id"
+    )
+    val releaseDates: List<TMKGReleaseDate>
+)
+
+@Entity
+data class TMKGReleaseDate(
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    @ColumnInfo(name = "game_id") val gameId: Long,
+    @ColumnInfo(name = "platform_name") val platformName: String,
+    @ColumnInfo(name = "release_date_human") val releaseDateHuman: String, // millis
+//    @ColumnInfo(name = "release_date") val releaseDate: Long, // millis
 )
