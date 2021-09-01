3d5c854613baa4ace855a2a104cc17a7f212ed74
Jordan Wilson <wilsonfraserjordan@gmail.com>
2021-09-01 08:07:01 -0700

    more dependency updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,58 +40,73 @@ android {
     }
 
     lint {
-        disable("ObsoleteSdkInt", "UnusedResources")
+        disable(
+            "ObsoleteSdkInt",
+            "UnusedResources",
+            "MediaCapabilities"
+        )
     }
 }
 
 dependencies {
-    val roomVersion = "2.3.0-beta01"
-    val lifecycleVersion = "2.3.0"
-    val glideVersion = "4.12.0"
-
     // Core
-    implementation("androidx.appcompat:appcompat:1.3.0-rc01")
-    implementation("androidx.core:core-ktx:1.3.2")
-    implementation("androidx.fragment:fragment-ktx:1.3.2")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.4.31")
+    val appcompatVersion = "1.3.1"
+    val coreVersion = "1.6.0"
+    val kotlinVersion = "1.5.20"
+    implementation("androidx.appcompat:appcompat:$appcompatVersion")
+    implementation("androidx.core:core-ktx:$coreVersion")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion")
 
     // DI
     val hiltVersion = "2.38.1"
     implementation("com.google.dagger:hilt-android:$hiltVersion")
     kapt("com.google.dagger:hilt-compiler:$hiltVersion")
-    // TODO figure out how to avoid overeager build error w/ below
-//    implementation("com.google.dagger:hilt-android:${rootProject.extra.get("hilt_version")}")
-//    kapt("com.google.dagger:hilt-compiler:${rootProject.extra.get("hilt_version")}")
 
     // Data
+    val roomVersion = "2.3.0"
+    val igdbApiVersion = "1.0.4"
+    val resultVersion = "4.0.0"
     kapt("androidx.room:room-compiler:$roomVersion")
     implementation("androidx.room:room-runtime:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
 
-    implementation("com.github.husnjak:IGDB-API-JVM:1.0.2")
-    implementation("com.google.protobuf:protobuf-java:3.13.0")
-    implementation("com.github.kittinunf.result:result:4.0.0")
-    implementation("com.github.kittinunf.result:result-coroutines:4.0.0")
+    implementation("com.github.husnjak:IGDB-API-JVM:$igdbApiVersion")
+    implementation("com.github.kittinunf.result:result:$resultVersion")
+    implementation("com.github.kittinunf.result:result-coroutines:$resultVersion")
 
     // UI
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    val fragmentVersion = "1.3.6"
+    val constraintLayoutVersion = "2.1.0"
+    val lifecycleVersion = "2.3.1"
+    val navigationVersion = "2.3.5"
+    val recyclerViewVersion = "1.2.1"
+    implementation("androidx.fragment:fragment-ktx:$fragmentVersion")
+    implementation("androidx.constraintlayout:constraintlayout:$constraintLayoutVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
-    // TODO check later if this is synced up again
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
-    implementation("androidx.recyclerview:recyclerview:1.2.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navigationVersion")
+    implementation("androidx.recyclerview:recyclerview:$recyclerViewVersion")
 
+    val glideVersion = "4.12.0"
+    val materialVersion = "1.4.0"
     implementation("com.github.bumptech.glide:glide:$glideVersion")
-    kapt("com.github.bumptech.glide:compiler:${glideVersion}")
-    implementation("com.google.android.material:material:1.3.0")
+    kapt("com.github.bumptech.glide:compiler:$glideVersion")
+    implementation("com.google.android.material:material:$materialVersion")
 
     // Testing
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 
+    // Misc.
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+}
+
+kapt {
+    correctErrorTypes = true
+    // look into this more (https://stackoverflow.com/a/68254606)
+    javacOptions {
+        option("-Adagger.hilt.android.internal.disableAndroidSuperclassValidation=true")
+    }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    val kotlinVersion by rootProject.extra("1.4.31")
-    val navigationVersion by rootProject.extra("2.3.3")
+    val kotlinVersion by rootProject.extra("1.5.20")
+    val navigationVersion by rootProject.extra("2.3.5")
     val hiltVersion by rootProject.extra("2.38.1")
     repositories {
         google()
@@ -10,9 +10,9 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.1")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navigationVersion")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
