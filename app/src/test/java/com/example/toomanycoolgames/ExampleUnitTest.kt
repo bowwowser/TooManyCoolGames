01be53d2a78542ab26d507c38ad0f41ecd4d4ff9
Jordan Wilson <wilsonfraserjordan@gmail.com>
2021-11-11 13:33:06 -0800

    GitHub Improvements
    -test: suppress warning with example addition test

--- app/src/test/java/com/example/toomanycoolgames/ExampleUnitTest.kt
+++ app/src/test/java/com/example/toomanycoolgames/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.example.toomanycoolgames
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,6 +11,7 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
+        @Suppress("INTEGER_OPERATOR_RESOLVE_WILL_CHANGE")
         assertEquals(4, 2 + 2)
     }
 }
